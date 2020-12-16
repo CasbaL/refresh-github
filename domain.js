dd411c28c09efedb3c491a01d6aa1f056cada426
casbal <yygf94half@163.com>
2020-12-16 14:40:16 +0800

    update: domain.js - 添加了几个域名

--- domain.js
+++ domain.js
@@ -1,5 +1,9 @@
 module.exports = ip => `
+${ip}  asset-cdn.github.com
+${ip}  cloud.githubusercontent.com
+${ip}  raw.githubusercontent.com
 ${ip}  camo.githubusercontent.com
+${ip}  user-images.githubusercontent.com
 ${ip}  avatars0.githubusercontent.com
 ${ip}  avatars1.githubusercontent.com
 ${ip}  avatars2.githubusercontent.com
